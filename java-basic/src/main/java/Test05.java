ee18252a5ea8603f5ec127336f5901b58667d458
seojung0 <seojung_7@naver.com>
2017-09-22 11:47:56 +0900

    'aa'

---
+++ java-basic/src/main/java/Test05.java
@@ -0,0 +1,14 @@
+class  Test05 {
+  public  static  void  main ( String [] args ) {
+   System.out.println ( " 안녕하세요 " );
+  }
+}
+/* 분리
+>jvac -d [.class 파일을 저장할 폴더][소스폴더명/../소스파일명]
+md src 
+javac -d bin crs -encoding utf8 Test03.java
+다른 폴더에 있는 클래스 실핼하기 java -classpath [.class 파일이 있는폴더 ]클래스명
+java -cp class 폴더 파일명 
+운영체제에 .classpath 를 사용하여 .class 파일이 있는 경로를 지정할 필요가없다.
+>set CLASSPATH=클래스파일이 있는 폴더
+*/
